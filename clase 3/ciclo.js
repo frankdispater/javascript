e72b91c2d18266a667a9bb1da9b4c383008503ce
frank <70403899+frankdispater@users.noreply.github.com>
2022-04-22 15:00:45 -0300

    Create ciclo.js

---
+++ clase 3/ciclo.js
@@ -0,0 +1,88 @@
+/* 
+for(desde; hasta; actualizacion){
+ lo que vamos a repetir
+}
+*/
+// i = i + 1 | esto para cambiar el numero
+
+/* for(let i = 0; i < 100; i++){
+    console.log("hola coder");
+} */
+
+/* for(let i = 0; i < 100; i++){
+    alert("hola coder");
+} */
+
+/* let ingresarNumero = parseInt(prompt("ingrese un numero"));
+
+for (let i = 0; i <= 10; i++){
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + " X " + i + " = "+ resultado);
+} */
+
+/* for(let i = 1; i <= 20; i++){
+    let nombre = prompt("ingresar nombre");
+
+    alert("tu nurmo es el numero " + i + "\nnombre: " + nombre);
+} */
+
+/* for(let i = 0; i < 10; i++){
+    console.log("iniciamos " + i);
+
+    if(i === 2){
+        console.log("entramos al if");
+        break;
+    }
+    console.log("terminamos");
+}
+console.log("terminamos proceso"); */
+
+/* for(let i = 0; i < 10; i++){
+    if(i == 3){
+        continue;
+    }
+
+    console.log("Terminamos " + i);
+} */
+
+/* while (condition) {
+    
+} */
+
+/* let repetir = true;
+
+while (repetir) {
+    console.log("hola");
+//esto crea un bucle infinito, hay que tener cuidado
+} */
+
+/* let entrada = prompt("ingresar un dato");
+
+while(entrada != "2"){
+    alert("ingresaste " + entrada);
+
+    entrada = prompt("ingresar un dato");
+} */
+
+/* let nombre = prompt("ingresa nombre");
+let contrasena = prompt("ingresa contrase;a");
+
+while(contrasena != "1234"){
+    alert("contrasena erronea");
+
+    contrasena = prompt("ingresar un dato");
+}  */
+
+/* let nombre = "andres";
+
+switch(nombre){
+    case "andres":
+    console.log("ingresaste andres");
+         break;
+    case "anita":
+        console.log("ingresaste anita");
+         break;
+    default:
+        console.log("no ingresaste ninguno");  
+         break
+} */
